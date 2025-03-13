3114a231e40b878086abc40369f510587872ee14
Michalis <michalisfot17@gmail.com>
2025-03-13 18:28:31 +0200

    Fix next.config.js syntax error

--- next.config.js
+++ next.config.js
@@ -1,16 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
 
-{
-  "extends": ["next/core-web-vitals", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint"],
-  "root": true,
-  "parserOptions": {
-    "project": "./tsconfig.json"
-  }
-}
